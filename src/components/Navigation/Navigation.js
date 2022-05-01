1aa58355c97f81ea8f6c08522703e2d7af62e763
ArnabShikder24 <arnabshikder2001@gmail.com>
2022-05-02 02:26:30 +0600

    singup ready

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -7,14 +7,12 @@ const Navigation = () => {
         <div>
             <Navbar className='py-4' bg="light" expand="lg">
                 <Container>
+                    <Link className='navbar-brand font-monospace' to="/home">Fab Fragrance</Link>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                    <Nav className="ms-auto">
                         <Link className='nav-link' to="/home">Home</Link>
                         <Link className='nav-link' to="/inventory">Inventory</Link>
-                    </Nav>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Link className='navbar-brand font-monospace' to="/home">Fab Fragrance</Link>
-                    <Nav className='ms-auto'>
                         <Link className='nav-link' to="/login">Login</Link>
                         <Link className='nav-link' to="/signup">Sign Up</Link>
                     </Nav>
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+import Loading from '../../components/Loading/Loading';
 
 const SignUp = () => {
+    const [ createUserWithEmailAndPassword, user, loading, error ] = useCreateUserWithEmailAndPassword(auth);
+    const [match, setMatch] = useState('')
+
+    // if(user) {
+    //     console.log(user)
+    // }
+
+    const signUphandle = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        const confirm = e.target.confirm.value;
+        if(password !== confirm) {
+            setMatch('Password Not Match! Please try again');
+            return
+        }
+        setMatch('');
+        createUserWithEmailAndPassword(email, password)
+    }
     return (
         <div className='login text-center my-5'>
             <div>
                 <h2>Sign Up</h2><div className='underline mb-4'></div>
-                <form>
+                <form onSubmit={signUphandle}>
                     <input className='w-25 mb-3 p-2' type="text" name='name' placeholder='Name' required/><br />
                     <input className='w-25 mb-3 p-2' type="email" name='email' placeholder='Email' required/><br />
                     <input className='w-25 p-2 mb-3' type="password" name='password' placeholder='Password' required/><br />
-                    <input className='w-25 p-2 mb-3' type="password" name='confirm' placeholder='Confirm Password' required/><br />
+                    <input className='w-25 p-2 mb-2' type="password" name='confirm' placeholder='Confirm Password' required/><br />
+                    <p className='text-danger'>{match}</p>
+                    <p className='text-danger'>{error && error.message}</p>
+                    <p>{loading && 'Loading...'}</p>
+                    <p className='text-success'>{user && 'Signup Successfull'}</p>
                     <input className='w-25 p-2' type="submit" value="Sign Up" />
                 </form>
                 <div className='or'>
